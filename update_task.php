85120993c962afa4658e323bf49d1ff2e23e0af1
isa-miho <isabella.miho@gmail.com>
2025-02-05 16:36:17 -0400

    fix(update_task): melhora do código de atualização

--- update_task.php
+++ update_task.php
@@ -1,10 +1,20 @@
 <?php
-        require_once 'conn.php';
-       
-        if($_GET['task_id'] != ""){
-                $task_id = $_GET['task_id'];
-               
-                $conn->query("UPDATE `task` SET `status` = 'Done' WHERE `task_id` = $task_id") or die(mysqli_errno($conn));
-                header('location: index.php');
-        }
+require_once 'conn.php';
+
+$task_id = filter_input(INPUT_GET, 'task_id', FILTER_SANITIZE_NUMBER_INT);
+$status_update = filter_input(INPUT_GET, 'status', FILTER_SANITIZE_STRING);
+
+if ($task_id && $status_update) {
+    $stmt = $conn->prepare("UPDATE `task` SET `status` = ? WHERE `task_id` = ?");
+    $stmt->bind_param("si", $status_update, $task_id);
+
+    if ($stmt->execute()) {
+        header("Location: index.php");
+        exit;
+    } else {
+        echo "Erro ao atualizar a tarefa.";
+    }
+} else {
+    echo "Parâmetros inválidos.";
+}
 ?>
